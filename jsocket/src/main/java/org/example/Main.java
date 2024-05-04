893dff2f15b0fba4a1cde29dbe27c0822ce83d67
am-shm <am.shahmoradi93@gmail.com>
2024-05-04 19:50:25 +0330

    main file added

---
+++ jsocket/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.example.Client;
+import org.example.Server;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Server server = new Server(8080);
+        //Client client = new Client("localhost",8080);
+    }
+}
