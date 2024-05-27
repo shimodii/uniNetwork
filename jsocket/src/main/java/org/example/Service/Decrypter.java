65a09f16c0a3a2496d3111537a829a59bc537c80
am-shm <am.shahmoradi93@gmail.com>
2024-05-27 10:44:03 +0330

    renamed variable for better reading

--- jsocket/src/main/java/org/example/Service/Decrypter.java
+++ jsocket/src/main/java/org/example/Service/Decrypter.java
@@ -2,11 +2,11 @@ package org.example.Service;
 
 public class Decrypter {
 
-    public String dmsg = new String();
+    public String decryptedMessage = new String();
     public Decrypter(String encryptedMessage){
         for (int i=0;i<encryptedMessage.length();i++){
             if (encryptedMessage.charAt(i) != '#')
-                dmsg += encryptedMessage.charAt(i);
+                decryptedMessage += encryptedMessage.charAt(i);
         }
     }
 }
