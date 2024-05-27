82c72807693dab52b841977e7d865c69f5cf0f42
am-shm <am.shahmoradi93@gmail.com>
2024-05-27 10:26:16 +0330

    var names changed for better reading

--- jsocket/src/main/java/org/example/Service/Encrypter.java
+++ jsocket/src/main/java/org/example/Service/Encrypter.java
@@ -1,11 +1,11 @@
 package org.example.Service;
 
 public class Encrypter {
-    public String Encrypter(String message){
-        String encryptedMessage = new String();
+
+    public String encryptedMessage = new String();
+    public Encrypter(String message){
         for (int i=0;i<message.length();i++){
             encryptedMessage += message.charAt(i) + "#";
         }
-        return encryptedMessage;
     }
 }
