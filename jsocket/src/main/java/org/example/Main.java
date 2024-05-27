37423b9e19d4960e9169d8f86910d639c31473af
am-shm <am.shahmoradi93@gmail.com>
2024-05-27 10:44:28 +0330

    user input works with 0 problem with enc and dec

--- jsocket/src/main/java/org/example/Main.java
+++ jsocket/src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 import org.example.Socket.*;
 import org.example.Service.*;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -13,11 +16,29 @@ public class Main {
 
 //        Server server = new Server(8080);
 //        Client client = new Client("localhost",8080);
-        Encrypter msg = new Encrypter("hello");
-        String emsg = msg.encryptedMessage;
-        System.out.println(emsg);
 
-        Decrypter nmsg = new Decrypter(emsg);
-        System.out.println(nmsg.dmsg);
+
+        /* manual for encrypting:
+        *   use as below
+        *
+        * */
+//        Encrypter msg = new Encrypter("hello");
+//        String emsg = msg.encryptedMessage;
+//        System.out.println(emsg);
+
+//        Decrypter nmsg = new Decrypter(emsg);
+//        System.out.println(nmsg.dmsg);
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your message: ");
+        String mainMessage = input.nextLine();
+        Encrypter msg = new Encrypter(mainMessage);
+        System.out.println(msg.encryptedMessage);
+        //ok
+
+        Decrypter dmsg = new Decrypter(msg.encryptedMessage);
+        System.out.println(dmsg.decryptedMessage);
+        //ok
     }
 }
