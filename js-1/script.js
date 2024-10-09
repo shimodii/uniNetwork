cae8af53b56968c76c29bc02bc0b22c018a4d43d
shimodi <am.shahmoradi93@gmail.com>
2024-10-09 21:48:33 +0330

    sign vars

--- js-1/script.js
+++ js-1/script.js
@@ -6,6 +6,12 @@ let image2 = document.getElementsByClassName("image2")
 let image3 = document.getElementsByClassName("image3")
 let image4 = document.getElementsByClassName("image4")
 
+//signs
+let sign1 = document.getElementsByClassName("step1-sign")
+let sign2 = document.getElementsByClassName("step2-sign")
+let sign3 = document.getElementsByClassName("step3-sign")
+let sign4 = document.getElementsByClassName("step4-sign")
+
 function counterUp(){
     counter++;
     if (counter >= 4) {
