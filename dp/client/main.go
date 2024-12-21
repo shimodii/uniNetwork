0021ff832784e3659eb0423e6365caf1dfd08c3c
shimodi <am.shahmoradi93@gmail.com>
2024-12-21 15:32:38 +0330

    server call for test

--- dp/client/main.go
+++ dp/client/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"net"
+)
+
 func main(){
-    println("hello from client")
+    //println("hello from client")
+    serverAddress := "localhost:8080"
+    connection, err := net.Dial("tcp", serverAddress)
+    if err != nil {
+        panic(err)
+    }
+    
+    msg := "hello from client"
+    connection.Write([]byte(msg))
 }
